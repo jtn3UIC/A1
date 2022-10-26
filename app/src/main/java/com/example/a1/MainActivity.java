c91b02dc8cec2b78c3aef2e799ca71a07fb9a2d9
Jordan Nguyen <jtn3@illinois.edu>
2022-10-26 16:12:27 -0500

    added button listner

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.a1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button hotel;
+    Button attract;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hotel = findViewById(R.id.hotel);
+        attract = findViewById(R.id.attract);
+
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(), "Send Hotel Intent", Toast.LENGTH_SHORT);
+                toast.show();
+                //System.out.println("")
+            }
+        });
+        attract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(), "Send Attract Intent", Toast.LENGTH_SHORT);
+                toast.show();
+                //System.out.println("")
+            }
+        });
     }
 }
