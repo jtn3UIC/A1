dc6847de2be7bbb165d3bcb8fbfc9bf7195ab8fd
jtn3 <jtn3@uic.edu>
2022-10-30 15:55:54 -0500

    granted stuck on

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
